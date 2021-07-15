667d17240bcaa412bd1e64eff026cec1650a1314
Ola Matre <Ola.Matre@ciber.no>
2021-07-15 13:00:33 +0200

    Create tables

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+import React from 'react'
+import { Home } from './components/home/Home'
+
+export const App = (): JSX.Element => {
+  return (
+    <Home />
+  )
+}
+
+export default App;
---
+++ src/components/home/Home.tsx
@@ -0,0 +1,63 @@
+import React, { Fragment } from 'react'
+
+const data = [
+    {
+        "event": "10 000m",
+        "group": "MS",
+        "athletes": [
+            {
+                "name": "Ola Matre",
+                "club": "Oslostudentenes Idrettslag"
+            },
+            {
+                "name": "Fredrik Fyksen Lund",
+                "club": "Oslostudentenes Idrettslag"
+            }
+        ]
+    },
+    {
+        "event": "10 000m",
+        "group": "KS",
+        "athletes": []
+    }
+]
+
+export const Home = (): JSX.Element => {
+
+    return (
+        <div>
+            <table style={{width: '100%'}}>
+                <tr>
+                    <th>Klasse</th>
+                    <th>Øvelse</th>
+                </tr>
+                {data?.map((element) => {
+                    return (
+                        <Fragment>
+                            <tr>
+                                <th>{element.group}</th>
+                                <th>{element.event}</th>
+                            </tr>
+                            <tr>
+                                <table style={{width: '100%'}}>
+                                    <tr>
+                                        <th>Navn</th>
+                                        <th>Klubb</th>
+                                    </tr>
+                                    {element.athletes?.map((athlete) => {
+                                        return (
+                                            <tr>
+                                                <th>{athlete.name}</th>
+                                                <th>{athlete.club}</th>
+                                            </tr>
+                                        )
+                                    })}
+                                </table>
+                            </tr>
+                        </Fragment>
+                    )
+                })}
+            </table>
+        </div>
+    )
+}
